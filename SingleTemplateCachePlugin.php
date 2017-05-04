a8df986e11033b8d23412dffd69efc46334cdf24
Joshua Martin <hello@joshuamartin.co.uk>
2017-05-04 09:46:48 +0100

    Boost version number

--- SingleTemplateCachePlugin.php
+++ SingleTemplateCachePlugin.php
@@ -10,7 +10,7 @@ class SingleTemplateCachePlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.0.3';
+        return '1.2.0';
     }
 
     public function getDeveloper()
